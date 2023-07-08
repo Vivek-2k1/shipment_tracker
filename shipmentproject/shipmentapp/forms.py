8deba8ab4a1039269f440d23d1a229782ad26ea6
Vivek Sharma <108932912+Vivek-2k1@users.noreply.github.com>
2023-07-09 00:41:56 +0530

    Add files via upload

--- shipmentproject/shipmentapp/forms.py
+++ shipmentproject/shipmentapp/forms.py
@@ -4,9 +4,6 @@ from .models import ShipmentModel,CargoModel,TrackingModel
 class ShipmentForm(forms.ModelForm):
     origin = forms.CharField(label='Origin')
     destination = forms.CharField(label='Destination')
-    expected_delivery_date = forms.DateField(label='Expected Delivery')
-    actual_delivery_date = forms.DateField(label='Actual Delivery')
-    status = forms.ChoiceField(label='Status')
     class Meta:
         model = ShipmentModel
         fields = ['origin','destination','expected_delivery_date','actual_delivery_date']
